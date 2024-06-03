366f956e825cde9d34a990c4ad23933a9d92e116
Yurismm <40805624+Yurismm@users.noreply.github.com>
2024-06-03 22:37:33 +0100

    add folders etc

--- Bot/bot.js
+++ Bot/bot.js
@@ -20,8 +20,8 @@ const client = new Client
 });
 
 client.commands = new Collection();
-const folderPath =path.join(__dirname, 'commands');
-const commandsFolders = fs.readdirSync(foldersPath);
+const foldersPath =path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
 
 // for the buttons later down the line, im using a lot of the old code from my old bot
@@ -32,15 +32,46 @@ const autorole = "";
 
 // add the commands from the commands folder WIP
 for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command);
-        } else {
-            console.warn(`The command at ${filePath} is missing a required "data" or "execute" property.`);
-        }
-    }
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
 }
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+
+});
+
+
+client.once(Events.ClientReady, cReady=> {
+    console.log(`logged in as ${cReady.user.tag}`);
+});
+
+client.login(token);
--- Bot/deploy-commands.js
+++ Bot/deploy-commands.js
@@ -5,6 +5,7 @@ const path = require('node:path');
 
 const commands = [];
 // Grab all the command files from the commands directory you created earlier
+
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
